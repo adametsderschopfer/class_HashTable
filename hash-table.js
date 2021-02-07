7836fdce3b68e1afb2c142b2b245804819d914fa
Adamets Vladislav <v.adamets@consult-info.ru>
2021-02-07 02:36:08 +0200

    chore: create class

---
+++ hash-table.js
@@ -0,0 +1,27 @@
+class HashTable {
+    #_store;
+    
+    constructor(_size = 13) {
+        this.#_store = new Map();
+        this._size = _size;
+    }
+
+    get dump() {
+        return this.#_store;
+    }
+
+    setElement() {
+        
+    }
+
+    getElement() {
+
+    }
+
+    hash() {
+        
+    }
+}
+
+const table = new HashTable();
+
