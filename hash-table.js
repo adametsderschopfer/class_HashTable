4597040cd921432ccbf9e36db01319c1bcdf99f9
Adamets Vladislav <v.adamets@consult-info.ru>
2021-02-09 22:04:59 +0200

    build: ht

--- hash-table.js
+++ hash-table.js
@@ -1,27 +1,65 @@
 class HashTable {
-    #_store;
-    
-    constructor(_size = 13) {
-        this.#_store = new Map();
-        this._size = _size;
+  constructor(_size = 13) {
+    this._store = [];
+    this._size = _size;
+  }
+
+  get dump() {
+    return this._store;
+  }
+
+  setElement(key, value) {
+    const idx = this.hash(key);
+
+    if (!this._store[idx]) {
+      this._store[idx] = [[key, value]];
+    } else {
+      const list = this._store[idx];
+      const matchingIdx = this.findMatchIndex(list, key);
+
+      if (matchingIdx) {
+        list[matchingIdx] = [key, value];
+      }
+
+      list.push([key, value]);
+    }
+  }
+  /* 
+      (this._store[idx] && this._store[idx][0] === key) */
+
+  getElement(key) {
+    if (this._store[this.hash(key)]) {
+      const list = this._store[this.hash(key)];
+      const index = this.findMatchIndex(list, key);
+
+      if (index !== undefined || index !== null) {
+        return list[index][1];
+      }
+    }
+  }
+
+  findMatchIndex(list, key) {
+    for (let i = 0; i < list.length; i++) {
+      if (list[i][0] === key) return i;
+    }
+  }
+
+  hash(string) {
+    let index = 0;
+
+    for (let i = 0; i < string.length; i++) {
+      index += string.charCodeAt(i) * i + 1;
     }
 
-    get dump() {
-        return this.#_store;
-    }
-
-    setElement() {
-        
-    }
-
-    getElement() {
-
-    }
-
-    hash() {
-        
-    }
+    return index % this._size;
+  }
 }
 
-const table = new HashTable();
+const ht = new HashTable();
 
+ht.setElement("boroda", "boroda");
+ht.setElement("winderton", "boroda13");
+ht.setElement("soer", "soer");
+console.table(ht.dump);
+
+console.log(ht.getElement("boroda"));
